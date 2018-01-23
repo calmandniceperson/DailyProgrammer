8b967a9551430b45547cb34abbf72ad037eaafc1
Michael Köppl <mko.mko31@gmail.com>
2018-01-23 23:13:04 +0100

    Clean code up and use snprintf instead of strcat

--- 321_Talking_Clock/main.c
+++ 321_Talking_Clock/main.c
@@ -14,50 +14,46 @@ const char *base = "It's ";
 
 const char *INPUT_FILE_PATH = "./input.txt";
 
-void attach_hour(char **dest, int *hour) {
-    if (*hour <= 12) {
-        strcat(*dest, number_words[*hour]);
+void attach_hour(char *dest, size_t buf_size, int hour) {
+    if (hour <= 12) {
+        snprintf(dest + strlen(dest), buf_size, "%s", number_words[hour]);
     } else {
-        strcat(*dest, number_words[*hour - 12]);
+        snprintf(dest + strlen(dest), buf_size, "%s", number_words[hour - 12]);
     }
 }
 
-void attach_minutes(char **dest, int *minutes) {
-    if (*minutes > 0) {
-        if (*minutes >= 20) {
+void attach_minutes(char *dest, size_t buf_size, int minutes) {
+    if (minutes > 0) {
+        if (minutes >= 20) {
             // Split the minute into two parts, so 31 becomes 3 and 1
             // This allows us to build the number string.
-            char *minute_parts = malloc(2);
-            sprintf(minute_parts, "%d", *minutes);
+            char minute_parts[2];
+            snprintf(minute_parts, sizeof(minute_parts), "%d", minutes);
             int major = minute_parts[0] - '0';
             int minor = minute_parts[1] - '0';
 
             // At the major part in any case.
-            strcat(*dest, " ");
-            strcat(*dest, minutes_ten_steps_words[major-2]);
+            snprintf(dest + strlen(dest), buf_size, " %s", minutes_ten_steps_words[major - 2]);
 
             // Add the minor part as well if it's not 0.
             if (minor > 0) {
-                strcat(*dest, " ");
-                strcat(*dest, number_words[minor]);
+                snprintf(dest + strlen(dest), buf_size, " %s", number_words[minor]);
             }
-        } else if (*minutes > 10 && *minutes < 20) {
-            strcat(*dest, " ");
-            strcat(*dest, number_words[*minutes]);
+        } else if (minutes > 10 && minutes < 20) {
+            snprintf(dest + strlen(dest), buf_size, " %s", number_words[minutes]);
         } else {
             // If we have a minute 0 < m <= 10, we put an 'oh' before the minute
             // string, as in "two oh five pm".
-            strcat(*dest, " oh ");
-            strcat(*dest, number_words[*minutes]);
+            snprintf(dest + strlen(dest), buf_size, " oh %s", number_words[minutes]);
         }
     }
 }
 
-void attach_am_pm(char **dest, int *hour) {
+void attach_am_pm(char *dest, int hour) {
     if (*hour <= 11) {
-        strcat(*dest, " am");
+        snprintf(dest + strlen(dest), 5, " am");
     } else {
-        strcat(*dest, " pm");
+        snprintf(dest + strlen(dest), 5, " pm");
     }
 }
 
@@ -76,12 +72,12 @@ int main(int argc, char **argv) {
         int hour = (line[0] - '0') * 10 + (line[1] - '0');
         int minutes = (line[3] - '0') * 10 + (line[4] - '0');
         
-        char *time_str = malloc(1024);
-        strcat(time_str, base);
+        char time_str[256];
+        snprintf(time_str, sizeof(time_str), "%s", base);
 
-        attach_hour(&time_str, &hour);   
-        attach_minutes(&time_str, &minutes);
-        attach_am_pm(&time_str, &hour);
+        attach_hour(time_str, sizeof(time_str), hour);   
+        attach_minutes(time_str, sizeof(time_str), minutes);
+        attach_am_pm(time_str, hour);
         
         printf("%s\n", time_str);
     }
